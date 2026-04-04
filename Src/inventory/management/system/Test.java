387cdface61d35d216ef0d5eab2f71bad8cdfa9b
Vishal Singh <vishalsingh7vz@gmail.com>
2026-04-04 08:06:12 +0530

    Add responsive login UI with admin image panel and clean build configuration

--- Src/inventory/management/system/Test.java
+++ Src/inventory/management/system/Test.java
@@ -2,7 +2,6 @@ package inventory.management.system;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, Inventory Management System!");
-        System.out.println("Project Setup Done");
+        new LoginFrame();
     }
 }
